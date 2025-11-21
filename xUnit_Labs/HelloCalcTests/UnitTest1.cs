82b9013a44c7c636ae6dfc7713c3d2460fa427f5
hamish <hamishmcintoshh@gmail.com>
2025-11-21 21:23:35 +1300

    Completed Green Phase: Created Calculator class

---
+++ xUnit_Labs/HelloCalcTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using HelloCalcApp; // This allows us to see the main app
+
+namespace HelloCalcTests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_GivenTwoNumbers_ReturnsSum()
+    {
+        // Arrange
+        var calc = new Calculator(); // This might show a red error line (Good!)
+
+        // Act
+        var result = calc.Add(2, 3);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
